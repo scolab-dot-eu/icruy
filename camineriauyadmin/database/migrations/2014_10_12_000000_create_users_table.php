de3e98c7923e95ee49a474b4c56a703500944408
Cesar Martinez Izquierdo <cesar.izq@gmail.com>
2018-12-24 14:08:16 +0100

    Soporte en modelo de datos de usuarios deshabilitados

--- camineriauyadmin/database/migrations/2014_10_12_000000_create_users_table.php
+++ camineriauyadmin/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('enabled')->default(True); // FIXME: currently not honoured for login
             $table->rememberToken();
             $table->timestamps();
         });
