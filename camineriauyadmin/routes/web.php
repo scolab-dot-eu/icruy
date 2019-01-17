37da84db5a15a00e52e1a1c48637ff0279ff9b18
Cesar Martinez Izquierdo <cesar.izq@gmail.com>
2019-01-17 20:44:48 +0100

    Las url de reinicio de contraseña deben ser públicas

--- camineriauyadmin/routes/web.php
+++ camineriauyadmin/routes/web.php
@@ -27,11 +27,6 @@ Route::prefix('dashboard')
         Route::resource('changerequests', 'ChangeRequestController', ['except' => [
             'show', 'create', 'store', 'destroy'
         ]]);
-        // Password Reset Routes...
-        Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-        Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-        Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-        Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
     Route::group(['middleware' => ['isadmin']], function() {
         
@@ -92,4 +87,9 @@ Route::group(['middleware' => ['auth']], function() {
 
 Route::get('/api/config/global', 'DepartmentConfigApiController@getGlobalConfig')->name('globalconfig');
 Route::post('/api/logout', 'Auth\LoginController@logout')->name('apilogout');
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
