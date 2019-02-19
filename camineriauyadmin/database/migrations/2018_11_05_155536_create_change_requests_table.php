8405747a049f3c3bd782fd4ef6cc72fa960d34b7
Cesar Martinez Izquierdo <cesar.izq@gmail.com>
2019-02-19 15:48:05 +0100

    Añadimos código de camino a changerequests

--- camineriauyadmin/database/migrations/2018_11_05_155536_create_change_requests_table.php
+++ camineriauyadmin/database/migrations/2018_11_05_155536_create_change_requests_table.php
@@ -18,6 +18,7 @@ class CreateChangerequestsTable extends Migration
             $table->integer('requested_by_id')->unsigned();
             $table->integer('validated_by_id')->unsigned()->nullable();
             $table->string('layer', 200);
+            $table->string('codigo_camino', 8)->nullable()->default(null);
             $table->integer('feature_id')->unsigned()->nullable();
             $table->string('departamento', 4);
             $table->string('status');
@@ -25,7 +26,8 @@ class CreateChangerequestsTable extends Migration
             $table->json('feature_previous')->nullable();
             $table->json('feature')->nullable();
             $table->timestamps();
-            $table->index(['status', 'layer', 'feature_id']);
+            $table->index(['status', 'layer', 'feature_id', 'codigo_camino'], 'idx_chr_sta_lyr_fea_cam');
+            $table->index(['status', 'layer', 'feature_id', 'departamento', 'codigo_camino'], 'idx_chr_sta_lyr_fea_dep_cam');
             $table->index(['requested_by_id', 'status', 'layer']);
             $table->index('validated_by_id');
             $table->foreign('departamento')->references('code')->on('departments');
